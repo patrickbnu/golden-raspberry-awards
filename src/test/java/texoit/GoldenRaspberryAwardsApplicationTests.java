792cc5d5a31223f3689240c72f515dc707bdc6c1
Patrick Nascimento <patrick.bnu@gmail.com>
2021-09-05 16:59:19 -0300

    first commit

---
+++ src/test/java/texoit/GoldenRaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package texoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
